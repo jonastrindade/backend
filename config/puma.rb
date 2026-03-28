b4eb81ffb51d4db195bf32c34751257f583037e9
Jonas Trindade <jonastrinda@gmail.com>
2026-03-28 03:19:20 -0300

    config puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,9 @@
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-if ENV["RAILS_ENV"] == "production"
-  directory "/var/www/backend/current"
-end
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+threads threads_count, threads_count
 
-rackup File.expand_path("config.ru", __dir__)
+workers ENV.fetch("WEB_CONCURRENCY", 2) if ENV["RAILS_ENV"] == "production"
 
 if ENV["RAILS_ENV"] == "production"
   pidfile "/var/www/backend/shared/tmp/pids/puma.pid"
@@ -15,21 +14,10 @@ if ENV["RAILS_ENV"] == "production"
   stdout_redirect "/var/www/backend/shared/log/puma.stdout.log",
                   "/var/www/backend/shared/log/puma.stderr.log",
                   true
-end
 
-if ENV["RAILS_ENV"] == "production"
   bind "unix:///var/www/backend/shared/tmp/sockets/puma.sock"
-else
-  port 3000
-end
 
-if ENV["RAILS_ENV"] == "production"
-  workers ENV.fetch("WEB_CONCURRENCY", 2)
-else
-  workers 0
+  preload_app!
 end
-
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
-threads threads_count, threads_count
 
 plugin :tmp_restart
