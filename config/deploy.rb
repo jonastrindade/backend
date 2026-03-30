1e3840cc7b10908a5d6301b2de9203fca27f33f5
Jonas Trindade <jonastrinda@gmail.com>
2026-03-29 23:37:21 -0300

    config capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,24 @@
-# config valid for current version and patch releases of Capistrano
 lock "~> 3.20.0"
 
 set :application, "backend"
 set :repo_url, "git@github.com:jonastrindade/backend.git"
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :branch, ENV.fetch("BRANCH", "main")
-
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/backend"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
 append :linked_files, "config/database.yml", "config/master.key"
-
-# Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
 set :ssh_options, {
   user: ENV.fetch("DEPLOY_USER", "deploy"),
   keys: %W[#{ENV['DEPLOY_KEY']}],
   forward_agent: true,
   auth_methods: %w[publickey]
 }
+namespace :puma do
+  desc "Restart Puma"
+  task :restart do
+    on roles(:app) do
+      execute :sudo, :systemctl, :restart, :puma
+    end
+  end
+end
+after "deploy:publishing", "puma:restart"
